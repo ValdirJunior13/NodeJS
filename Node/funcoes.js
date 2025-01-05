4267dfe0e5ef1b581c6697a29090007aa26ae6ad
ValdirJunior13 <v.zacarias@hotmail.com.br>
2025-01-05 13:48:45 -0300

    organizando

--- Node/funcoes.js
+++ Node/funcoes.js
@@ -1,55 +1,19 @@
 const express = require("express");
 const app = express();
+const { engine } = require('express-handlebars');
+const Sequelize = require('sequelize');
 
-app.get("/", function(req, res){
-    res.sendFile(__dirname + "/Html/base.html");
-})
-app.get("/", function(req, res){
-    res.send("Seja bem-vindo ao meu app");
-});
-app.get("/sobre", function(req, res){
-    res.sendFile(__dirname + "/Html/sobre.html")
-})
+app.engine('handlebars', engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars')
 
-app.get("/sobre", function(req, res){
-    res.send("Minha página sobre");
-});
-
-app.get("/blog", function(req, res){
-    res.send("Bem-vindo ao meu blog!");
-});
-
-app.get('/ola/:cargo/:nome/:cor', function(req, res){
-    res.send(req.params);
-});
-
-const Sequelize = require('sequelize')
-const sequelize = new Sequelize('SistemaDeCadastro', 'root', 'Senha', {
-    host: "localhost", 
+const sequelize = new Sequelize('SistemaDeCadastro', 'root', 'Omegamon37871145887', {
+    host: "localhost",
     dialect: 'mysql'
 
 })
-
-sequelize.authenticate().then(function(){
-    console.log("Conectado")
-}).catch(function(erro){
-    console.log("Falha ao se conectar: " + erro)
+app.get('/cad', function(req, res){
+    res.send('Rota de Cadastro de Post')
 })
-
-const Postagem = sequelize.define('SistemaDeCadastro', {
-    titulo: {
-        type: Sequelize.STRING 
-    }, 
-    conteudo:{
-        type: Sequelize.TEXT
-    }
-})
-
-
-Postagem.sync({force:true})
-
 app.listen(8081, function(){
-    console.log("servidor rodando");
+    console.log("Servidor rodando na url localhost:8081");
 });
-
-
