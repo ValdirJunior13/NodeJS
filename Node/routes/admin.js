fb2b5a82d20e29e55adcbb6fcfb16288a1efdfda
ValdirJunior13 <v.zacarias@hotmail.com.br>
2025-01-16 16:22:19 -0300

    Correcoes

--- Node/routes/admin.js
+++ Node/routes/admin.js
@@ -129,7 +129,7 @@ router.get("/postagens", (req, res) =>{
   
 })
 router.get('/postagens/add', (req, res) =>{
-    Categoria.find().then((categorias) => {
+    Categoria.find().lean().then((categorias) => {
         res.render("admin/addpostagem", {categorias: categorias})
     }).catch((err) => {
         req.flash("error_msg", "Houve um erro interno ao carregar o formulário")
